3eb338be3a7949e573a3adb1c512d7fd20868351
rynlienerth <lienerth@hartford.edu>
2023-09-17 13:18:40 -0400

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -1,7 +1,46 @@
 // Import libraries here
 
-public class Assignment1 {
-  public static void main(String[] args) {
-    // Add your code here
-  }
+public class BlockLetters {
+ 
+  public static void printBlockLetterK() {
+   String[] letterK = { 
+  "KK        KK",
+  "KK      KK",
+  "KK"   KK",
+  "KK   KK",
+  "KK KK",
+  "KKKK",
+  "KK KK",
+  "KK   KK", 
+  "KK"   KK",
+  "KK      KK",
+  "KK        KK",
+  };
+  for (String line : letterK) {
+    Syste,.out.println(line);
 }
+  }
+
+
+  public static void printBlockLetterL() {
+    String[] letterL = {
+      "LL",
+      "LL",
+      "LL",
+      "LL",
+      "LL",
+      "LL",
+      "LL",
+      "LL""LL""LL",
+    };
+    for (String line : letterL) {
+      System.out.println(line);
+    }
+  }
+    public static void main(String[] args){
+      printBlockLetterK();
+      System.out.println();
+      printBlockLetterL();
+    }
+
+    
