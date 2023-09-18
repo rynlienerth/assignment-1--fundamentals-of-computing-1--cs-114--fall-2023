4dc33676713222220fd32be70747a9b02e0f1134
rynlienerth <lienerth@hartford.edu>
2023-09-18 15:25:59 -0400

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -21,7 +21,7 @@ public class Assignment1 {
   "KK        KK LLLLLLLLLLLLLL",
   };
   for (String line : letterKL) {
-    System.out.println(line);
+          System.out.println(line);
 }
   }
       
@@ -31,30 +31,37 @@ public class Assignment1 {
 
   public static void main(String[] args) {
     
+    //printing block letters
+              printBlockLetterKL(); 
+              
+    //Start of step 2 Temperature Conversions   
     try (Scanner scanner = new Scanner(System.in)) {
-      System.out.println("Enter temperature in Fahrenheit: ");
-      double fahrenheit = scanner.nextDouble();
-      System.out.println("Enter a 5-character string: ");
+             System.out.println("Enter temperature in Fahrenheit: ");
+    double fahrenheit = scanner.nextDouble();
+             System.out.println("Enter a 5-character string: ");
 String inputString = scanner.next();
-      double celsius = (fahrenheit - 32) * 5 / 9;
+    double celsius = (fahrenheit - 32) * 5 / 9;
 
-      printBlockLetterKL(); 
-      System.out.println("Temperature Conversion: " + fahrenheit + " degrees Fahrenheit is equal to " + celsius + " degrees Celsius.");
+      
+              
+      //printing temperature conversion
+              System.out.println("Temperature Conversion: " + fahrenheit + " degrees Fahrenheit is equal to " + celsius + " degrees Celsius.");
  
       // Step 3 Trimmed Reverse String
   
-
+//adding just in case string is not 5 letters in length
  if (inputString.length() != 5) {
-      System.out.println("Error: Please enter a 5-character string.");
+            System.out.println("Error: Please enter a 5-character string.");
       return;
  }
  String trimmedReversedString = new StringBuilder(inputString.substring(1,4)).reverse().toString();
 
- System.out.println("Result: " + trimmedReversedString);
+ //Printing out the trimmed reversed string
+            System.out.println("Result: " + trimmedReversedString);
   
  //Added Step 4 Random Number
  Random random = new Random();
- int randomNumber = random.nextInt(16353) + 32;
+      int randomNumber = random.nextInt(16353) + 32;
             System.out.println("Random Number: " + randomNumber);
   
   //Step 5 adding TempConversion, Reversed trimmed string, and random number
