24f86ab55fe6c5223a2135d3cfd771e222c8ae59
rynlienerth <lienerth@hartford.edu>
2023-09-20 17:05:10 -0400

    Updating white space and got rid of things on the left

--- Assignment1.java
+++ Assignment1.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Assignment1 {
  
   public static void printBlockLetterKL() {
-   String[] letterKL = { 
+    String[] letterKL = { 
  
   "KK      KK   LL            ",
   "KK    KK     LL            ",
@@ -20,50 +20,50 @@ public class Assignment1 {
   "KK      KK   LL            ",
   "KK        KK LLLLLLLLLLLLLL",
 
-  };
-  for (String line : letterKL) {
-          System.out.println(line);
+};
+    for (String line : letterKL) {
+    System.out.println(line);
 }
-  }
-//Start of step 2 Temperature Conversions
-
+}
+    //Start of step 2 Temperature Conversions
   public static void main(String[] args) {
-    
+   
     //printing block letters
-              printBlockLetterKL(); 
+     printBlockLetterKL(); 
               
     //Start of step 2 Temperature Conversions   
+   
     try (Scanner scanner = new Scanner(System.in)) {
-             System.out.println("Enter temperature in Fahrenheit: ");
+    System.out.println("Enter temperature in Fahrenheit: ");
+    
     double fahrenheit = scanner.nextDouble();
-             System.out.println("Enter a 5-character string: ");
-String inputString = scanner.next();
+    System.out.println("Enter a 5-character string: ");
+    
+    String inputString = scanner.next();
     double celsius = (fahrenheit - 32) * 5 / 9;
-
-      
-              
-      //printing temperature conversion
-              System.out.println("Temperature Conversion: " + fahrenheit + " degrees Fahrenheit is equal to " + celsius + " degrees Celsius.");
+        
+    //printing temperature conversion
+    System.out.println("Temperature Conversion: " + fahrenheit + " degrees Fahrenheit is equal to " + celsius + " degrees Celsius.");
  
-      // Step 3 Trimmed Reverse String
+    // Step 3 Trimmed Reverse String
   
-//adding just in case string is not 5 letters in length
- if (inputString.length() != 5) {
-            System.out.println("Error: Please enter a 5-character string.");
-      return;
- }
- String trimmedReversedString = new StringBuilder(inputString.substring(1,4)).reverse().toString();
+    //adding just in case string is not 5 letters in length
+    if (inputString.length() != 5) {
+    System.out.println("Error: Please enter a 5-character string.");
+    return;
+}
+    String trimmedReversedString = new StringBuilder(inputString.substring(1,4)).reverse().toString();
 
- //Printing out the trimmed reversed string
-            System.out.println("Result: " + trimmedReversedString);
+    //Printing out the trimmed reversed string
+    System.out.println("Result: " + trimmedReversedString);
   
- //Added Step 4 Random Number
- Random random = new Random();
-      int randomNumber = random.nextInt(16353) + 32;
-            System.out.println("Random Number: " + randomNumber);
+    //Added Step 4 Random Number
+    Random random = new Random();
+    int randomNumber = random.nextInt(16353) + 32;
+    System.out.println("Random Number: " + randomNumber);
   
-  //Step 5 adding TempConversion, Reversed trimmed string, and random number
-            System.out.print(celsius+trimmedReversedString+randomNumber);
-          }
+    //Step 5 adding TempConversion, Reversed trimmed string, and random number
+    System.out.print(celsius+trimmedReversedString+randomNumber);
+  }
   }
 }
